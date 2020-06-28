33067329322e4c8a2feba1e34594a9ac731297e8
emanuelbas <emanuelbastons@gmail.com>
2020-06-28 06:33:13 -0300

    Empiezo con el controlador para agregar un producto

--- frontend/src/app/modules/admin/stock/stock-cargar-producto-individual/stock-cargar-producto-individual.component.ts
+++ frontend/src/app/modules/admin/stock/stock-cargar-producto-individual/stock-cargar-producto-individual.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Validators } from '@angular/forms';
+import { Producto, ProductoApi, TipoProducto, TipoProductoApi, Categoria, CategoriaApi } from '../../../../_services/lbservice';
+
 
 @Component({
   selector: 'app-stock-cargar-producto-individual',
@@ -7,9 +12,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StockCargarProductoIndividualComponent implements OnInit {
 
-  constructor() { }
+	form: FormGroup;
+	fechaActual = new Date();
+	producto: Producto;
 
-  ngOnInit() {
-  }
+
+	constructor(
+		private router:Router,
+		private productoApi:ProductoApi,
+		private tipoApi:TipoProductoApi,
+		private categoria:CategoriaApi
+		)
+	{ 
+		this.form = new FormGroup({
+			nombre: new FormControl('', [Validators.required]),
+			categoria: new FormControl('', [Validators.required]),
+			cant: new FormControl('', [Validators.required]),
+			vto: new FormControl('', [Validators.required]),
+			peso: new FormControl('', [Validators.required]),
+      	});
+
+      	//Estos serian los datos que quiero obtener del form
+      	//y mandar a la api
+		this.producto.cantidad;
+		this.producto.vencimiento;
+		this.producto.tipoProducto.nombre;
+		this.producto.tipoProducto.categoria;
+		this.producto.tipoProducto.peso;
+	}
+
+	ngOnInit() {
+	}
 
 }
