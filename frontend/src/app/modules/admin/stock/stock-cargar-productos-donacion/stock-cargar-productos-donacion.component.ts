427b8538e92c5cc20c3beb892c2691de12f80bf9
emanuelbas <emanuelbastons@gmail.com>
2020-07-01 14:28:51 -0300

    Agregar donacion a stock funcionando

--- frontend/src/app/modules/admin/stock/stock-cargar-productos-donacion/stock-cargar-productos-donacion.component.ts
+++ frontend/src/app/modules/admin/stock/stock-cargar-productos-donacion/stock-cargar-productos-donacion.component.ts
@@ -13,13 +13,14 @@ import { Donacion, DonacionApi, Producto, ProductoApi, TipoProducto, TipoProduct
 export class StockCargarProductosDonacionComponent implements OnInit {
 
 	form: FormGroup;
+	formDonacion: FormGroup;
 	fechaActual = new Date();
 	producto: Producto;
 	categorias: Categoria[];
 
 	//Donaciones que no esten desarmadas ni enviadas en una donacion
 	donacionesNuevas : Donacion[];
-	productosPrecargados:Producto[];
+	productosPrecargados:Producto[] = [];
 
 	constructor(
 		private router:Router,
@@ -29,6 +30,10 @@ export class StockCargarProductosDonacionComponent implements OnInit {
 		private donacionApi:DonacionApi
 		//apiDonacion
 		) {
+
+		this.formDonacion = new FormGroup({
+			donacion: new FormControl('',[Validators.required])
+		});
 
 		this.form = new FormGroup({
 			nombre: new FormControl('', [Validators.required]),
@@ -45,13 +50,14 @@ export class StockCargarProductosDonacionComponent implements OnInit {
 		
 		}
 
-	precargar(){
+	submitProduct(){
 		this.producto = new Producto;
 		this.producto.tipoProducto = new TipoProducto;
 
 		this.producto.tipoProducto.nombre = this.form.get("nombre").value;
 		this.producto.tipoProducto.peso = this.form.get("peso").value;
 		this.producto.tipoProducto.categoriaId = this.form.get("categoria").value;
+		alert(this.producto.tipoProducto.categoriaId);
 		this.producto.cantidad = this.form.get("cant").value;
 		this.producto.vencimiento = this.form.get("vto").value;
 
@@ -62,4 +68,23 @@ export class StockCargarProductosDonacionComponent implements OnInit {
 	ngOnInit() {
 	}
 
+	submitAll(donacion){
+
+
+		//Hago un array para registrar los tipoProducto
+		let tipoProductos:TipoProducto[] = []; 
+		for (let i=0; i< this.productosPrecargados.length;i++){
+			tipoProductos[i] = this.productosPrecargados[i].tipoProducto;
+		}
+
+		//Registro los tipoProducto y obtengo su ID, entonces registro los productos
+		this.tipoApi.createMany(tipoProductos).subscribe((res:TipoProducto[])=>{
+			for (let i=0; i<this.productosPrecargados.length;i++){
+				this.productosPrecargados[i].tipoProductoId = res[i].id;
+			};
+			this.productoApi.createMany(this.productosPrecargados).subscribe(()=>{
+				alert("Se crearon todos los productos y sus tipo productos");
+			});
+		});
+	}
 }
