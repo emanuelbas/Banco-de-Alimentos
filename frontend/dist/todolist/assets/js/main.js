c90332843dcbe84b0aafe1206d165b1332728c11
emanuelbas <emanuelbastons@gmail.com>
2020-08-24 16:47:59 -0300

    Mejora docker 🐳

--- frontend/dist/todolist/assets/js/main.js
+++
@@ -1,6 +0,0 @@
-function toggleClasses(contentToShow,contentToHide,classes){
-	for (CSSClass of classes) {
-	contentToHide.classList.remove(CSSClass);
-	contentToShow.classList.add(CSSClass);
-	}
-}
