b810b6882a29060d9797373563afe6797a632af0
emanuelbas <emanuelbastons@gmail.com>
2020-08-26 12:21:01 -0300

    Fix categorias duplicadas

--- backend/server/boot/init_models.js
+++ backend/server/boot/init_models.js
@@ -15,47 +15,51 @@ module.exports = function (app, cb) {
   }], function (err, users) {
     if (err) return err;
     console.log('Created users:', users);
+    //Si crea el admin además agrega las categorias
+    var Categoria = app.models.Categoria;
+    Categoria.create([
+      {
+        "nombre": "golosinas"
+      },
+      {
+        "nombre": "congelados"
+      },
+      {
+        "nombre": "refrigerados"
+      },
+      {
+        "nombre": "manufacturados por BALP"
+      },
+      {
+        "nombre": "no perecederos"
+      },
+      {
+        "nombre": "lácteos"
+      },
+      {
+        "nombre": "leche en polvo"
+      },
+      {
+        "nombre": "bebida"
+      },
+      {
+        "nombre": "frutas y verduras"
+      },
+      {
+        "nombre": "higiene y limpieza"
+      },
+      {
+        "nombre": "pañales"
+      }
+    ], function (err, categories) {
+        if (err) return err;
+        console.log('Added default categories: ', categories);
+      });
+
+
   });
 
-  var Categoria = app.models.Categoria;
-  Categoria.create([
-  {
-    "nombre": "golosinas"
-  },
-  {
-    "nombre": "congelados"
-  },
-  {
-    "nombre": "refrigerados"
-  },
-  {
-    "nombre": "manufacturados por BALP"
-  },
-  {
-    "nombre": "no perecederos"
-  },
-  {
-    "nombre": "lácteos"
-  },
-  {
-    "nombre": "leche en polvo"
-  },
-  {
-    "nombre": "bebida"
-  },
-  {
-    "nombre": "frutas y verduras"
-  },
-  {
-    "nombre": "higiene y limpieza"
-  },
-  {
-    "nombre": "pañales"
-  }
-], function (err, categories) {
-    if (err) return err;
-    console.log('Added default categories: ', categories);
-  });
+  
   
   /* Con estas lineas se activan los fixures, los quito porque resetean la base
   app.loadFixtures() .then(function() {console.log('Done!');})
