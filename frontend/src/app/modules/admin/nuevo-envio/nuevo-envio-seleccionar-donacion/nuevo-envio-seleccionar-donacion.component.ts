455d437e3c8babee0fe324eea1eca43862747c77
emanuelbas <emanuelbastons@gmail.com>
2020-09-04 17:31:50 -0300

    Agrego descargar archivo al crear envio

--- frontend/src/app/modules/admin/nuevo-envio/nuevo-envio-seleccionar-donacion/nuevo-envio-seleccionar-donacion.component.ts
+++ frontend/src/app/modules/admin/nuevo-envio/nuevo-envio-seleccionar-donacion/nuevo-envio-seleccionar-donacion.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
 import { Donacion} from '../../../../_services/lbservice/models';  
 import { DonacionApi} from '../../../../_services/lbservice/services';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-nuevo-envio-seleccionar-donacion',
@@ -10,6 +11,7 @@ import { DonacionApi} from '../../../../_services/lbservice/services';
 export class NuevoEnvioSeleccionarDonacionComponent implements OnInit {
 
 	donaciones = [];
+	base = environment.backendUrl;
 
 	@Input()  name: string;
   	@Output() enviarIdDonacion = new EventEmitter<string>();
@@ -19,9 +21,8 @@ export class NuevoEnvioSeleccionarDonacionComponent implements OnInit {
 		//Tengo que obtener todas las donaciones que no pertenecen a un envio
 		//Es decir todas las que tengan idEnvio = null 
 		//Otra opcion investigar como pasar funciones para filtrar los donantes que no tienen donaciones -> donanteApi.find({order: 'username ASC', include:{relation:'donaciones', scope:{fields:['estado','id'],where:{"estado":"nueva"}, include:{relation:'traslado'}}}}).subscribe((donaciones:Donante[])=>{
-		donacionApi.find({fields: {idDonante:true,id:true},include:[{relation:'donante', scope:{fields:['username']}},{relation:'traslado',scope:{fields:['descripcion']}}],where:{"estado":"nueva"}}).subscribe((donaciones:Donacion[])=>{
+		donacionApi.find({fields: {idDonante:true,id:true,filename:true},include:[{relation:'donante', scope:{fields:['username']}},{relation:'traslado',scope:{fields:['descripcion']}}],where:{"estado":"nueva"}}).subscribe((donaciones:Donacion[])=>{
 			this.donaciones = donaciones;
-			console.log(this.donaciones);
 		})
 	}
 
